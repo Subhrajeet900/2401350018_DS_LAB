c4c5cb938ac72a0df43bf39f83fe09d570dd8eda
subhrajeet dash <subhrajeetdash900@gmail.com>
2025-11-25 21:07:16 +0530

    Implement linear search algorithm in Java

---
+++ 9_LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+ public static int linearSearch(int[] arr, int key) {
+ for (int i = 0; i < arr.length; i++) {
+ if (arr[i] == key) {
+ return i;
+ }
+ }
+ return -1;
+ }
+ public static void main(String[] args) {
+ int[] arr = {5, 3, 8, 4, 2};
+ System.out.println(linearSearch(arr, 8));
+ System.out.println(linearSearch(arr, 10));
+ }
+}
